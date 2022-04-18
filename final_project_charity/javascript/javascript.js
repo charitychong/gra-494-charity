2911deeebdf512e65f2f4a2c1cb9a89874b5e7d1
charitychong <70442303+charitychong@users.noreply.github.com>
2022-04-18 02:19:23 -0700

    submit to lightbox not wokring

--- final_project_charity/javascript/javascript.js
+++ final_project_charity/javascript/javascript.js
@@ -1,19 +1,66 @@
 
-function myFunction() {
-
-
-
-    var images=['imgs/001.jpg',
-                'imgs/002.jpg',
-                'imgs/003.jpg',
-                'imgs/004.jpg',
-                'imgs/005.jpg',];
-    
-    var randomNumber = Math.floor(Math.random() * images.length);
-    var bgImg = 'url(' + images[randomNumber] + ')';
-    
-    $('body').css({'background':bgImg, 'background-size':'cover', });
-    
+  document.addEventListener('DOMContentLoaded', () => {
+    const openBtn = document.querySelector('.video-thumb');
+    const closeBtn = document.querySelector('.close');
+    const overlay = document.querySelector('.video-overlay');
+  
+    openBtn.addEventListener('click', () => {
+      overlay.style.display = 'block';
     });
+  
+    closeBtn.addEventListener('click', () => {
+      overlay.style.display = 'none';
+    });
+  
+  });
 
 
+  document.addEventListener('DOMContentLoaded', () => {
+    const openBtn2 = document.querySelector('.video-thumb2');
+    const closeBtn2 = document.querySelector('.close2');
+    const overlay2 = document.querySelector('.video-overlay2');
+  
+    openBtn2.addEventListener('click', () => {
+      overlay2.style.display = 'block';
+    });
+  
+    closeBtn2.addEventListener('click', () => {
+      overlay2.style.display = 'none';
+    });
+  
+  });
+
+
+
+  document.addEventListener('DOMContentLoaded', () => {
+    const openBtn3 = document.querySelector('.video-thumb3');
+    const closeBtn3 = document.querySelector('.close3');
+    const overlay3 = document.querySelector('.video-overlay3');
+  
+    openBtn3.addEventListener('click', () => {
+      overlay3.style.display = 'block';
+    });
+  
+    closeBtn3.addEventListener('click', () => {
+      overlay3.style.display = 'none';
+    });
+  
+  });
+
+
+  document.addEventListener('DOMContentLoaded', () => {
+    const submit = document.querySelector('#submit');
+    const closeBtn4 = document.querySelector('.close4');
+    const contactformoverlay = document.querySelector('.contactform-overlay');
+  
+    submit.addEventListener('click', () => {
+      contactformoverlay.style.display = 'block';
+    });
+  
+    closeBtn4.addEventListener('click', () => {
+      contactformoverlay.style.display = 'none';
+    });
+  
+  });
+  
+  
