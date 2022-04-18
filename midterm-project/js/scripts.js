098bb9c3bd5c6b45c31f161436cf3aeb717491f4
charitychong <70442303+charitychong@users.noreply.github.com>
2022-04-18 02:28:30 -0700

    missing files - javascript

--- midterm-project/js/scripts.js
+++ midterm-project/js/scripts.js
@@ -1,1 +1,39 @@
-// Add scripts here
+
+function myFunction() {
+
+    var x = document.getElementById("highlight1__callout--content");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const openBtn = document.querySelector('.video-thumb');
+  const closeBtn = document.querySelector('.close');
+  const overlay = document.querySelector('.video-overlay');
+
+  openBtn.addEventListener('click', () => {
+    overlay.style.display = 'block';
+  });
+
+  closeBtn.addEventListener('click', () => {
+    overlay.style.display = 'none';
+  });
+
+
+});
+
+
+
+
+
+
+
+
+
+
